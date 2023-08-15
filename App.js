6697ff6f23264d466745ec8b8823cd06c2f5d396
hachad-14 <hachadsaad14@gmail.com>
2023-08-16 00:32:56 +0100

    test

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{
+        top: -320,
+        width: "100%",
+        height: 120,
+        backgroundColor: "#535353",
+        borderRadius: 20,
+      }}>
+        <Text style={{
+          fontSize: 20,
+          textAlign: 'center',
+          top:80,
+          color: "#fff",
+          fontWeight: "bold",
+        }}>Bienvenue Saad.</Text>
+      </View>
+      <Text style={{
+        color: "#fff",
+        fontSize: 20,
+        textAlign: 'center',
+        paddingBottom: 100,
+      }}>Avant d'appuyer sur le boutton est tu sur de ton choix ?</Text>
+      <Button
+        onPress={() => Alert.alert('Votre choix à été la cause de la mort de 30K de vies.')}
+        title="Activer"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,8 +37,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#000',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
